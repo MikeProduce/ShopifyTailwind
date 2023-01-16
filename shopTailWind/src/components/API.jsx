574f8304f6053744e4374c2e299c66314b68114d
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-01-16 13:16:34 -0700

    trying to display the data

--- shopTailWind/src/components/API.jsx
+++ shopTailWind/src/components/API.jsx
@@ -12,15 +12,25 @@ export const API = () => {
   useEffect(() => {
     axios.get('https://api.bluecartapi.com/request?api_key=CBB74B236EBA47FA9A1C76EAEE4C240D&search_term=Clothes&type=search')
     .then(response => {
-      setData(response.data.search_results[0].product.title);
+      setData(response.data.search_results);
       setLoading(false);
-      console.log(response.data.search_results[0].product.title);
+      console.log(response.data.search_results);
     })
     .catch(error => {
       setError(error)
       setLoading(false);
     })
   },[])
+  //found the price and object for the item now i just have to parse into it 
+  //  and display the data.
+  const extractedData = data.map((item) => {
+    // console.log(item.product);
+    let Obj = {
+      // title: item.product.title.title,
+      // img : item.images,
+    }
+    console.log(Obj)
+  })
 
   if (loading) {
     return <p>Loading...</p>}
@@ -31,20 +41,19 @@ export const API = () => {
 
   return (
     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-    {data.map((product, key) => (
-      <div key="key">{product} </div>
-      // <a key={product.id} href={product.href} className="group">
-      //   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-      //     <img
-      //       src={product.imageSrc}
-      //       alt={product.imageAlt}
-      //       className="h-full w-full object-cover object-center group-hover:opacity-75"
-      //     />
-      //   </div>
-      //   <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-      //   <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-      // </a>
-    ))}
+    {/* {data.map((product) => (
+      <a key={product.id} href={product.href} className="group">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <img
+            src={product.imageSrc}
+            alt={product.imageAlt}
+            className="h-full w-full object-cover object-center group-hover:opacity-75"
+          />
+        </div>
+        <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+      </a>
+    ))} */}
   </div>
     )
 }
