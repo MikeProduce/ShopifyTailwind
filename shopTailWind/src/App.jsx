ff04732aab1d83c2a8308bbbe8bcdd8deca0f8f9
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-01-15 18:43:29 -0700

    done for today i guess

--- shopTailWind/src/App.jsx
+++ shopTailWind/src/App.jsx
@@ -2,30 +2,36 @@ import React from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route,Link,Outlet,RouterProvider} from 'react-router-dom'
 import {Home} from './home.jsx'
 import {Pay} from './pay.jsx'
+import {Confirmation} from './confirmation'
 
- function App(props) {
+const Root = () => {
+  return (
+    <div>
+      <nav className="flex justify-center bg-gray-800" >
+        <Link to="/"> Home </Link>
+        <Link to="/pay"> Check Out</Link>
+        <Link to="/confirmation">Confirmation</Link>
+      </nav>
+        <Outlet />
+    </div>)
+}
+
+ function App() {
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Root />}>
         <Route index element={<Home />} />
-        <Route element={<Pay />} />
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/confirmation" element={<Confirmation />} />
       </Route>
     )
   )
   return (
     <div className="w-screen h-screen bg-red-300">
-      <RouterProvider router={router}/>
+      <RouterProvider  router={router}/> 
     </div>
   )
 }
-const Root = () => {
-  return (
-  <nav>
-    <Link to="/"> Home </Link>
-    <Link to="/pay"> Check Out  </Link>
-  <div>
-    <Outlet />
-  </div>
-  </nav>)
-}
+
 export default App
