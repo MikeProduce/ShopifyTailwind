b5bf2a7e9805ca12cda79ea66e0a913d7d5a6084
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-01-17 08:08:27 -0700

    trying to pass on a counter components

--- shopTailWind/src/components/Navbar.jsx
+++ shopTailWind/src/components/Navbar.jsx
@@ -6,21 +6,23 @@ export const Root = () => {
   const [isOpen,setIsOpen] = useState(false);
 
 
+
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   }
   const linksClass  = isOpen ? "": "hidden"
+  const bigNumber = null
 
 
   return (
     <div>
-      <div className='flex'>
-        <nav className="relative w-full bg-gray-800 text-white justify-end" >
+      <div>
+        <nav className="w-full bg-gray-800 text-white" >
           <button className='block text-white md:hidden p-4' onClick={toggleOpen}>
             <i className="fa-solid fa-bars fa-2x"></i>
           </button>
-        <ul className={`bg-gray-800 md:flex md:items-center md:w-auto ${linksClass} text-xl font-medium text-white-800`}>
-         <li className='p-4 hover:bg-gray-400'><Link to="/pay"><i className={`fa-solid fa-cart-shopping fa-2x block`}></i></Link></li>
+        <ul className={`bg-gray-800 md:flex  md:items-center md:w-auto ${linksClass} text-xl text-white-800`}>
+         <li className='p-4 hover:bg-gray-400 relative'><Link to="/pay"><i className={`fa-solid fa-cart-shopping fa-2x block`}><span className='absolute left-8 top-4 text-red-600 text-xl'>3</span></i></Link></li>
           <li className='p-4 hover:bg-gray-400'><Link to="/"> Home </Link></li>
           <li className='p-4 hover:bg-gray-400'><Link to="/confirmation">Confirmation</Link></li>
         </ul>
