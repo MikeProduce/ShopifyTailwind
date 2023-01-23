ebef15ce3fa31ced0c87b80244e01ff89dcc33eb
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-01-22 19:18:06 -0700

    almost done with the tailwind in this pay page

--- shopTailWind/src/Pages/pay.jsx
+++ shopTailWind/src/Pages/pay.jsx
@@ -6,35 +6,45 @@ import { addToCart } from '../app/cartSlice.jsx';
 
 
 
-export const Pay = (props) => {
+export const Pay = () => {
   const {products,cart,total} = useSelector((state) => state.cart)
 
   const itemsInCart = cart.reduce((acc, item) => {
+    //the parameters are an empty array and the item which represets what were getting from redux. which is the image,name,price
     const existingItem = acc.find(i => i.itemName === item.itemName);
+    //here we are using find key word to loop through the array to see if something matches.(ex: miguel === miguel)
     if (existingItem) {
         existingItem.quantity += 1;
     } else {
         acc.push({...item, quantity: 1});
     }
+    //this if else statement checks if the array contains the item name, if it does contain the item name then itll add +1 to the quantity else it will push that new item into the array as an object
+    console.log(acc)
     return acc;
+    //here we are just returning the array so that we can map through it and display the information.
 }, []);
 
  
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-       <h1>{total}</h1>
+       <h1 className='text-lg'>Shopping Cart</h1>
        {itemsInCart.map((item, index) => (
         <ul key={index}>
-          <li>
-            <div>{item.itemName}</div>
-            <div>{item.itemPrice}</div>
-            <img src={item.itemImage} alt="" />
-            <div>Quantity: {item.quantity}</div>
+          <li className='flex flex-wrap p-4 sm:justify-center lg:justify-center md:justify-center'>
+            <div className="p-2">
+              <img className='w-full h-auto sm:w-1/2 lg:w-1/3' src={item.itemImage} alt="" />
+            </div> 
+            <div className="p-2">
+              <div>{item.itemName}</div>
+              <div>{item.itemPrice}</div>
+              <div>Quantity: {item.quantity}</div>
+            </div>
           </li>
         </ul>
       ))}
       </div>
+      {total}
     </div>
   )
 }
