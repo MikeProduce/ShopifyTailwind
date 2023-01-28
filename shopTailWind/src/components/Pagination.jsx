b914951c0a3fff96c4eb91fccff65cddbc3a255f
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-01-28 07:13:36 -0700

    added comments to understand pagination code

--- shopTailWind/src/components/Pagination.jsx
+++ shopTailWind/src/components/Pagination.jsx
@@ -3,22 +3,29 @@ import { Button } from './Button'
 
 export const Pagination = ({totalPosts,postsPerPage,setCurrentPage,pageSelected}) => {
   let pages = []
+  // this page declares an empty array of pages that will be used to store pages that will be rendered in the pagination UI
   const [pageRange, setPageRange] = useState([1, 3]);
+  //this line declares a page range using the useState hook
 
   for (let i = pageRange[0]; i <= pageRange[1]; i++){
     pages.push(i)
+    console.log(pages);
+    // this line pushes the current value of i to the pages array created on line 2
   }
+  //this loop iterates from the first value of page range and the second value of page range. pageRange[0] = this is the first item in the array, pageRange[1] is the second 
 
   const handleNextPage = () => {
     if(pageRange[1] + 1 <= Math.ceil(totalPosts/postsPerPage))
+    // this line checks if the next page range value is less than or equal to the toal number of pages.
     setPageRange([pageRange[0] + 1, pageRange[1] + 1]);
+    //this page updates the pageRange by one for [0], and [1].
   }
 
   const handlePrevPage = () => {
     if(pageRange[1] + 1 <= Math.ceil(totalPosts/postsPerPage))
     setPageRange([pageRange[0] - 1, pageRange[1] - 1]);
   }
-
+ 
   return (
     <div className='flex justify-center items-center p-4'>
     <button className={`bg-gray-800 text-white rounded-md hover:bg-opacity-50 px-4 py-2 rounded-md text-lg px-6 py-3 mt-10`} onClick={handlePrevPage}>Prev</button>
